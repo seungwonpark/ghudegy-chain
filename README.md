01cba813097bc467f807be25e0db8c368eb0d359
seungwon park <yyyyy@snu.ac.kr>
2019-03-31 11:55:27 +0900

    genesis commit aaaaaf

---
+++ README.md
@@ -0,0 +1,12 @@
+# kudeki-chain
+kudeki-chain inspired by https://github.com/dj-shin/gitchain
+
+## Merging pull request without merge commit
+```bash
+git checkout -b jh05013-master master
+git pull https://github.com/jh05013/kudeki-chain.git master
+git checkout master
+git merge --ff-only jh05013-master # not --no-ff
+git push origin master
+```
+Refer to https://shinglyu.com/web/2018/03/25/merge-pull-requests-without-merge-commits.html
---
+++ manage.py
@@ -0,0 +1,27 @@
+import subprocess
+
+
+def check_work():
+    commit_num = subprocess.check_output("git rev-list HEAD --count", shell=True).decode('utf-8').strip()
+    commit_num = int(commit_num)
+
+    sha1 = subprocess.check_output("git rev-parse HEAD", shell=True).decode('utf-8').strip()
+    work = len(sha1) - len(sha1.lstrip('0'))
+
+    print(f"Number of commits done: {commit_num}")
+    print(f"SHA1 hash of latest commit: {sha1}")
+    print(f"Difficulty of work done in commit: {work}")
+    assert commit_num <= work, "Not enough work done."
+
+def check_text():
+    with open('success.txt', 'r') as f:
+        lines = f.readlines()
+    assert len(lines) == 1, "success.txt must have only 1 line"
+
+    line = lines[0].strip()
+    assert len(line) <= 100, "Username must be shorter than 100 characters"
+
+if __name__ == '__main__':
+    check_text()
+    check_work()
+
